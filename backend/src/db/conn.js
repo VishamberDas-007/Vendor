2f2bbe1f3bba0fac5d30c23d354dadf3edb4553e
Vishamber <vishdas1111@gmail.com>
2022-10-02 16:39:41 +0530

    Used mongodb cloud services

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,19 +1,17 @@
-const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/vendor";
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+const mongoose = require("mongoose"); // importing the mongoose package
 
-const db = mongoose.connection;
-const Schema = mongoose.Schema;
-// console.log({ db });
-db.once("open", (_) => {
-	console.log("Database connected:", url);
-});
+const url =
+	"mongodb+srv://visdas:mFkgT780NygeinS7@cluster0.bvlifu1.mongodb.net/vendor?retryWrites=true&w=majority"; // initializing the url
 
-db.on("error", (err) => {
-	console.error("connection error:", err);
-});
-
-module.exports = { mongoose };
+// connecting to the mongoose db
+mongoose
+	.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Database connected:", url);
+	})
+	.catch((error) => {
+		console.log("connection error:", err);
+	});
