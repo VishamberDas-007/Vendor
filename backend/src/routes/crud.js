bccd9432032c16a49affe189a5f346589a69742b
Vishamber <vishdas1111@gmail.com>
2022-10-05 21:52:16 +0530

    delete is in progress

---
+++ backend/src/routes/crud.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const routes = express.Router();
+const { insert } = require("../controllers/crud/crud");
+const { deleting } = require("../controllers/crud/crud");
+const { listing } = require("../controllers/crud/crud");
+
+// post to insert the category
+routes.post("/insert", async (req, res) => {
+	let result = await insert(req);
+	return res.status(result.status).json(result);
+});
+
+// delete to delete the category
+routes.delete("/delete/:id", async (req, res) => {
+	let result = await deleting(req);
+	return res.status(result.status).json(result);
+});
+
+// list to list the category details
+routes.get("/listing", async (req, res) => {
+	let result = await listing(req);
+	return res.status(result.status).json(result);
+});
+
+module.exports = routes;
