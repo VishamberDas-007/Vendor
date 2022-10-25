01a92e7bf3b1ee8e5ecc623683b7749c6ab1d22e
Vishamber <vishdas1111@gmail.com>
2022-10-25 11:50:12 +0530

    Added login route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,17 @@
 const express = require("express"); // importing the express package
 const routes = express.Router(); // initializing routes
 const { register } = require("../controllers/auth"); // importing the register from the controller
+const { login } = require("../controllers/auth"); // importing the login from the controller
 
 // post request for the registeration of the vendor
 routes.post("/register", async (req, res) => {
 	let result = await register(req);
+	return res.status(result.status).json(result);
+});
+
+// get request for the registeration of the vendor
+routes.get("/login", async (req, res) => {
+	let result = await login(req);
 	return res.status(result.status).json(result);
 });
 
