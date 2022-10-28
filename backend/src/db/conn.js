56aca56a86f71ec8b4794da2b274841f367175d1
Vishamber <vishdas1111@gmail.com>
2022-10-28 16:50:10 +0530

    Changes made

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose"); // importing the mongoose package
 require("dotenv").config();
 
 const category = process.env.URL1; // initializing the url
-const vendor = process.env.URL2; // initializing the url
+const vendor =
+	"mongodb+srv://visdas:mFkgT780NygeinS7@cluster0.bvlifu1.mongodb.net/vendor?retryWrites=true&w=majority"; // initializing the url
+
+console.log({ vendor });
 
 // connecting to the mongoose db
 mongoose
